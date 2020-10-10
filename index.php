b5b111bfde0055e8a834a46aad6152f482ea8705
Amir Etemad <amiretemad2000@gmail.com>
2020-10-11 01:52:04 +0300

    index.php was removed.

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include "vendor/autoload.php";
-
-
-$service = new \Isolta\OpenData\Service\BusinessService(new \Isolta\OpenData\Formatter\BusinessResponseFormatter());
-
-dd($service->fetch('3163643-1'));
